7dbb6c1b873292771ee590c7302fa59bc85c4537
Filip Krikava <krikava@gmail.com>
2017-10-03 18:19:21 +0200

    Updated screen dimming

--- .config/i3/scripts/dim-screen.sh
+++ .config/i3/scripts/dim-screen.sh
@@ -4,7 +4,16 @@ N=${1:-5}
 CURR=$(brightnessctl -m g | cut -d',' -f 3)
 STEP=$(( $CURR / $N ))
 
+function restore {
+    echo "Restoring to $CURR"
+    brightnessctl s $CURR
+}
+
+trap restore SIGINT SIGTERM SIGKILL
+
 for i in $(seq 1 $N); do
     brightnessctl s $STEP-
     sleep 1
 done
+
+restore
