20ac6331f52190a7b7e4c4b96d2e988133590b29
Filip Krikava <krikava@gmail.com>
2018-05-30 11:22:06 +0200

    Updated the dropdown script

--- bin/x-terminal-dropdown.sh
+++ bin/x-terminal-dropdown.sh
@@ -1,16 +1,28 @@
-#!/bin/bash
+#!/bin/bash -x
 
-function move_window() {
+function position_window() {
     wid=$1
 
     eval $(xdotool getwindowgeometry --shell $wid)
-    eval $(xwininfo -root | awk '/Width/{w=$2} /Height/{h=$2} END {print "D_WIDTH="w"\nD_HEIGHT="h}')
-    x=$((D_WIDTH/2-WIDTH/2))
-    y=$((D_HEIGHT/2-HEIGHT/2))
+    eval $(xwininfo -root | awk '/Width/{w=$2} /Height/{h=$2} END {print "screen_width="w"\nscreen_height="h}')
+    x=$((screen_width/2-WIDTH/2))
+    y=$((screen_height/2-HEIGHT/2))
 
-    echo "Moving window $wid to $x $y"
+    if [ ! $x -eq $X -o ! $y -eq $Y ]; then
+         xdotool windowmove --sync $wid $x $y
+         echo "Moving window $wid from [$X, $Y] to [$x, $y]"
+    fi
+}
 
-    xdotool windowmove $wid $x $y
+function move_window_to_current_desktop() {
+    wid=$1
+
+    window_desktop=$(xdotool get_desktop_for_window $wid)
+
+    if [ $window_desktop -ne $current_desktop ]; then
+        echo "Moving window $wid from desktop $window_desktop to $current_desktop"
+        xdotool set_desktop_for_window $wid $current_desktop
+    fi
 }
 
 function find_window() {
@@ -22,6 +34,7 @@ function start() {
     (setsid /home/krikava/bin/x-terminal.sh -e tmux attach-session -t dropdown >& /dev/null) & disown
 }
 
+current_desktop=$(xdotool get_desktop)
 wid=$(find_window)
 
 if [ -z "$wid" ]; then
@@ -40,17 +53,26 @@ if [ -z "$wid" ]; then
         exit 1
     fi
 
-    move_window $wid
+    position_window $wid
 else
-    if [ -z "$(find_window --onlyvisible)" ]; then
-        echo "Showing $wid"
-        xdotool windowmap $wid
-        xdotool windowfocus $wid
-        move_window $wid
+    visible_wid=$(find_window --onlyvisible)
+    if [ -z $visible_wid ]; then
+        xdotool get_desktop_for_window $wid || xdotool windowmap --sync $wid
+        window_desktop=$(xdotool get_desktop_for_window $wid 2>/dev/null)
+
+        if [ $current_desktop -ne $window_desktop ]; then
+            echo "Moving $wid"
+            move_window_to_current_desktop $wid
+            position_window $wid
+            xdotool windowfocus --sync $wid
+        else
+            echo "Showing $wid"
+            move_window_to_current_desktop $wid
+            position_window $wid
+            xdotool windowfocus --sync $wid
+        fi
     else
         echo "Hiding $wid"
-        xdotool windowunmap $wid
+        xdotool windowunmap --sync $wid
     fi
 fi
-
-
