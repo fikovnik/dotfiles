1c0097e595fa67417c94269fb14878a89e2b9ac0
Filip Krikava <krikava@gmail.com>
2018-11-30 11:20:28 +0100

    Moved to rofi window selection

--- bin/x-window-menu.sh
+++ bin/x-window-menu.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-rofi -show window -show-icons -p "> "
+rofi -show window \
+     -show-icons \
+     -p "> " \
+     -kb-accept-entry '!Super-Tab,!Super-ISO_Left_Tab,!Super-Up,!Super-Down,Return' \
+     -kb-row-down 'Super+Tab,Super+Down,Down' \
+     -kb-row-up 'Super+ISO_Left_Tab,Super+Up,Up'
