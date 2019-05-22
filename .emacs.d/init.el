f1e6db55e5626f11c014377ee3cc1efdafc99f93
Filip Krikava <krikava@gmail.com>
2019-05-22 21:54:26 +0200

    emacs: new bootstrap

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,7 @@
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(require 'org)
+
+(let ((config-file (expand-file-name "config.org" user-emacs-directory))
+      (init-file (expand-file-name "init.el" user-emacs-directory)))
+
+  (org-babel-tangle-file config-file init-file "emacs-lisp")
+  (load-file init-file))
