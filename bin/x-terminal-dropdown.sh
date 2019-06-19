e67882d88c3ae062f5c9cd8ed64c724fb3c42e2a
Filip Krikava <krikava@gmail.com>
2019-06-19 16:45:03 +0200

    update x-terminal-dropdown to work with new st -d setting

--- bin/x-terminal-dropdown.sh
+++ bin/x-terminal-dropdown.sh
@@ -1,7 +1,3 @@
 #!/bin/sh -x
 
-if tmux list-sessions | grep dropdown | grep attached; then
-    i3-msg "scratchpad show"
-else
-    $HOME/bin/x-terminal.sh tmux new-session -A -s dropdown
-fi
+$HOME/bin/x-terminal.sh -d -n dropdown tmux new-session -A -s dropdown
