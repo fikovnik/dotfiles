1b368507c37515473b914d5b842fab0ed9030d13
Filip Krikava <krikava@gmail.com>
2020-02-07 11:45:31 +0100

    emacs: create extra profile for doom

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,2 +1,4 @@
-(("my"       . ((user-emacs-directory . "~/.my-emacs.d")))
- ("default"  . ((user-emacs-directory . "~/.doom-emacs.d"))))
+(("my"         . ((user-emacs-directory . "~/.my-emacs.d")))
+ ("default"    . ((user-emacs-directory . "~/.doom-emacs.d")))
+ ("doom"       . ((user-emacs-directory . "~/.doom-emacs.d")))
+ ("spacemacs"  . ((user-emacs-directory . "~/.spacemacs-emacs.d"))))
