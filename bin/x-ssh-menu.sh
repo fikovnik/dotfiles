7ce452622b13a3cbd7e5b8fdb245d9bf9fb74a82
Filip Krikava <krikava@gmail.com>
2020-03-10 21:57:34 +0100

    rofi: make ssh-menu connect to tmux

--- bin/x-ssh-menu.sh
+++ bin/x-ssh-menu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rofi -show ssh -show-icons -p "> " -ssh-command '/home/krikava/bin/x-terminal.sh -e sh -c " SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket) ssh {host}"'
+rofi -show ssh -ssh-command '/home/krikava/bin/x-terminal.sh -e sh -c " SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket) ssh {host} -t tmux new-session -A -s main"'
