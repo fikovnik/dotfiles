4b4eb62d08eb9224b03a3a81143ef7f1e0d8e8e5
Filip Krikava <krikava@gmail.com>
2020-05-25 11:20:43 +0200

    email: updated email services and configuration

--- bin/sync-email.sh
+++ bin/sync-email.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
-notmuch new
-if [ $? -ne 0 ]; then
-  notify-send "There was a problem updating notmuch"
-  exit 1
+function error_tty {
+  >&2 echo "$@"
+}
+
+function error_notify {
+  notify-send "$@"
+}
+
+if [ -n "$TERM" ]; then
+  offlineimap_ui="ttyui"
+  error=error_tty
+else
+  offlineimap_ui="ttyui"
+  error=error_notify
 fi
+
+/usr/bin/offlineimap -o -u $offlineimap_ui
+exitval=$?
+if [ $exitval -ne 0 ]; then
+   "$error" "There was a problem syncing email"
+   exit $exitval
+fi
+
+#notmuch new
+#if [ $? -ne 0 ]; then
+#  notify-send "There was a problem updating notmuch"
+#  exit 1
+#fi
