22ed7af4403038d436a3eee5f0a1aec7f94defb3
Filip Krikava <krikava@gmail.com>
2020-08-10 12:04:52 +0200

    Fix laptop screen setting script

--- .screenlayout/laptop-1080p.sh
+++ .screenlayout/laptop-1080p.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --off --output DP2 --off
+xrandr --output DP-1 --off
+xrandr --output DP-2 --off
+xrandr --output DP-1-1 --off
+xrandr --output DP-1-2 --off
+xrandr --output DP-1-3 --off
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output DP-1-1 --off
