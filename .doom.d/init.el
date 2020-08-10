f5809d76236a1fe95de3aa0f1b6f2c334ebe54b5
Filip Krikava <krikava@gmail.com>
2020-08-10 12:06:46 +0200

    emacs: add zen module

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -51,7 +51,7 @@
        (window-select     ; visually switch windows
         +numbers)
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
