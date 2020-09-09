d2c617da1728871c82f319b40c7fa95085a2f8d5
Filip Krikava <krikava@gmail.com>
2020-09-09 10:13:01 +0200

    Set SSH_AUTH_SOCK for ssh-menu

--- bin/x-ssh-menu.sh
+++ bin/x-ssh-menu.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#rofi -show ssh -ssh-command '/home/krikava/bin/x-terminal.sh -e sh -c " SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket) ssh {host} -t tmux new-session -A -s main"'
-rofi -show ssh -ssh-command '/home/krikava/bin/x-terminal.sh -e zsh -c "ssh {host} -t tmux new-session -A -s main"'
+rofi -show ssh -ssh-command '/home/krikava/bin/x-terminal.sh -e sh -c "export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket); ssh {host} -t tmux new-session -A -s main"'
