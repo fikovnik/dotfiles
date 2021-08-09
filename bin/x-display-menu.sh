ed6586f3d939b4975a1683dd342e2b93b8339221
Filip Krikava <krikava@gmail.com>
2021-08-09 10:37:43 +0200

    update wallpaper

--- bin/x-display-menu.sh
+++ bin/x-display-menu.sh
@@ -9,7 +9,9 @@ LAYOUT=$(find $DIR -type f | sed 's=.*/==;s=\.[^.]*$==' | rofi -dmenu -p "Screen
 $DIR/$LAYOUT.sh
 
 if [ -z "$XDG_CURRENT_DESKTOP" ]; then
-  feh --bg-scale ~/Documents/Pictures/Wallpapers/dolomites.jpg
+  res=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')
+  background="~/.local/share/backgrounds/dolomites-$(res).jpg"
+  [ -f "$background" ] && feh --bg-scale "$background"
   ~/.config/polybar/launch.sh
 fi
 
