0eb1887b9b05d7c616a530735236b29700da3144
Filip Krikava <krikava@gmail.com>
2022-01-07 16:57:45 +0100

    Fix dropdown terminal commands

--- bin/x-dropdown.sh
+++ bin/x-dropdown.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-echo $1
-if [ $# -lt 1 ]; then
-    echo "Usage: $0 name [ command ]"
-    exit 1
-fi
-
-name=$1
-shift
-
-$HOME/bin/x-terminal.sh -n $name tmux new-session -A -s $name "$@"
-tdrop -ma -w 50% -h 50% -y 25% -x 25% ~/bin/x-terminal-dropdown.sh
+~/Projects/mde/dropdown/target/debug/dropdown /usr/local/bin/st "$@"
