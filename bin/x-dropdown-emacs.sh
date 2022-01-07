1d6309f0e98e0c649a699ae38933d65e8baebd84
Filip Krikava <krikava@gmail.com>
2022-01-07 16:57:45 +0100

    Fix dropdown terminal commands

--- bin/x-dropdown-emacs.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-$HOME/bin/x-dropdown.sh emacs emacs -nw
--- bin/x-dropdown-terminal.sh
+++ bin/x-dropdown-terminal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -x
 
-$HOME/bin/x-dropdown.sh dropdown
+$HOME/bin/x-dropdown.sh -t dropdown -g 142x42 tmux new-session -A -s dropdown
