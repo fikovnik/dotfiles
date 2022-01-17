bb0e7ae0a7ddf142a10fc9814cbf4aacf4c79d61
Filip Krikava <krikava@gmail.com>
2022-01-17 15:52:45 +0100

    Remove my-emacs

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,4 +1,3 @@
-(("my"         . ((user-emacs-directory . "~/.my-emacs.d")))
- ("default"    . ((user-emacs-directory . "~/.doom-emacs.d")))
+(("default"    . ((user-emacs-directory . "~/.doom-emacs.d")))
  ("doom"       . ((user-emacs-directory . "~/.doom-emacs.d")))
  ("spacemacs"  . ((user-emacs-directory . "~/.spacemacs-emacs.d"))))
