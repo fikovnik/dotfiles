2246eefae5ee2ce15537a657ab1e26b2ba5411d6
Filip Krikava <krikava@gmail.com>
2022-10-24 10:27:45 +0200

    nvim: fixes

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -2,10 +2,11 @@ local status_ok, aerial = pcall(require, 'aerial')
 if not status_ok then return end
 
 aerial.setup {
-  backends = { 'lsp', 'treesitter', 'markdown' },
   layout = {
     min_width = 20,
+    placement = "edge",
   },
+  attach_mode = "global",
   show_guides = true,
   filter_kind = false,
 }
