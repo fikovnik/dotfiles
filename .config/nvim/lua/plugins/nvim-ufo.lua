fa9484ae2108e1b56096d272ea4f32cce467a6be
Filip Krikava <krikava@gmail.com>
2022-11-21 08:41:21 +0100

    nvim: remove the foldcolumn

--- .config/nvim/lua/plugins/nvim-ufo.lua
+++ .config/nvim/lua/plugins/nvim-ufo.lua
@@ -7,7 +7,7 @@ if not (vim.fn.has('nvim-0.7.0') > 0) then
   return
 end
 
-vim.o.foldcolumn = '1'
+vim.o.foldcolumn = '0'
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
