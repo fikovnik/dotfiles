1df815f8a3d0d83b2c1414e4ef7e11770ae2d8c5
Filip Krikava <krikava@gmail.com>
2023-01-08 22:36:14 +0100

    nvim: open file at the last location

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -67,3 +67,14 @@ autocmd('BufEnter', {
   pattern = { 'term://*' },
   callback = function() vim.cmd 'startinsert!' end,
 })
+
+-- visit last location
+autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
