183b8cf1586a6b5a40160b330de47c4031a36a91
Filip Krikava <krikava@gmail.com>
2023-01-15 22:17:12 +0100

    Add helper scripts

---
+++ bin/x-dropdown-journal.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+$HOME/bin/x-dropdown.sh -t journal -g 142x42 tmux new-session -A -s journal nvim '+WikiJournal'
---
+++ bin/x-dropdown-notes.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+$HOME/bin/x-dropdown.sh -t notes -g 142x42 tmux new-session -A -s notes nvim '+WikiFzfPages'
