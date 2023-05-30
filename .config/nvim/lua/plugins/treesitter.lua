5317299eb6da4cdc0427c3a7994d96e864efd286
Filip Krikava <krikava@gmail.com>
2023-05-30 12:21:12 +0000

    nvim: fix comments

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -14,7 +14,6 @@ return {
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
-      context_commentstring = { enable = true, enable_autocmd = false },
       ensure_installed = {
         "bash",
         "vimdoc",
