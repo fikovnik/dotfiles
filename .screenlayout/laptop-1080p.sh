d78eb309a3505a72ef3aa7c9e89f8c1db0008250
Filip Krikava <krikava@gmail.com>
2023-06-07 10:51:54 +0200

    Cleanup

--- .screenlayout/laptop-1080p.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xrandr --output DP-1 --off
-xrandr --output DP-2 --off
-xrandr --output DP-1-1 --off
-xrandr --output DP-1-2 --off
-xrandr --output DP-1-3 --off
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
-xrandr --output DP-1-1 --off
