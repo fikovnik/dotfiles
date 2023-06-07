e5a92ac31c12234217b24c12e918896a5eabc2b5
Filip Krikava <krikava@gmail.com>
2023-06-07 07:31:20 +0000

    nvim: dressing

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -9,10 +9,10 @@ return {
         return vim.ui.select(...)
       end
       -- ---@diagnostic disable-next-line: duplicate-set-field
-      -- vim.ui.input = function(...)
-      --   require("lazy").load({ plugins = { "dressing.nvim" } })
-      --   return vim.ui.input(...)
-      -- end
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
     end,
   },
   {
