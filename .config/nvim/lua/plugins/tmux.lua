09d1b770f73384ab35402b242524144c11b92150
Filip Krikava <krikava@gmail.com>
2023-06-07 07:26:03 +0000

    nvim: add slime

--- .config/nvim/lua/plugins/tmux.lua
+++ .config/nvim/lua/plugins/tmux.lua
@@ -1,3 +1,5 @@
+local Util = require("util")
+
 return {
   {
     "aserowy/tmux.nvim",
@@ -76,5 +78,30 @@ return {
         enable_default_keybindings = false,
       },
     },
+  },
+  {
+    "jpalardy/vim-slime",
+    keys = {
+      { mode = "x", "<C-c><C-c>", "<Plug>SlimeRegionSend", desc = "Send region to tmux" },
+      { mode = "n", "<C-c><C-c>", "vib" .. Util.cmd("SlimeSend"), desc = "Send block to tmux" },
+      { mode = "n", "<C-c><C-l>", Util.cmd("SlimeSendCurrentLine"), desc = "Send line to tmux" },
+      { mode = "n", "<leader>xx", "<Plug>SlimeMotionSend", desc = "Send to tmux" },
+      { mode = "n", "<leader>xc", Util.cmd("SlimeConfig"), desc = "Config" },
+    },
+    init = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>x"] = { name = "+execute" },
+      })
+    end,
+    config = function(_, opts)
+      vim.g.slime_no_mappings = 1
+      vim.g.slime_target = "tmux"
+      vim.g.slime_default_config = {
+        socket_name = "default",
+        target_pane = "{last}",
+      }
+      vim.g.slime_dont_ask_default = 0
+    end,
   },
 }
