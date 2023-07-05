5eb8b0e02f615893d02df4e30cadb3e1d9f06ab4
Filip Krikava <krikava@gmail.com>
2023-07-05 22:06:04 +0200

    nvim: disable treesitter indent

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -14,7 +14,7 @@ return {
     },
     opts = {
       highlight = { enable = true },
-      indent = { enable = true },
+      indent = { enable = false },
       ensure_installed = {
         "bash",
         "vimdoc",
