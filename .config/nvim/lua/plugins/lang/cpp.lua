8a59698d3080b9ba373633a832b2566117aed96d
Filip Krikava <krikava@gmail.com>
2023-07-13 16:16:37 +0000

    nvim: better debugging

--- .config/nvim/lua/plugins/lang/cpp.lua
+++ .config/nvim/lua/plugins/lang/cpp.lua
@@ -26,9 +26,22 @@ return {
     opts = {
       servers = {
         clangd = {
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
           cmd = {
             "clangd",
-            "--offset-encoding=utf-16",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=llvm",
+          },
+          init_options = {
+            usePlaceholders = true,
+            completeUnimported = true,
+            clangdFileStatus = true,
           },
         },
       },
@@ -43,5 +56,43 @@ return {
         end,
       },
     },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      -- Ensure C/C++ debugger is installed
+      "williamboman/mason.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if type(opts.ensure_installed) == "table" then
+          vim.list_extend(opts.ensure_installed, { "codelldb", "cpptools" })
+        end
+      end,
+    },
+    opts = function()
+      local dap = require("dap")
+      for _, lang in ipairs({ "c", "cpp" }) do
+        dap.configurations[lang] = {
+          {
+            type = "codelldb",
+            request = "launch",
+            name = "Launch file (codelldb)",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "codelldb",
+            request = "attach",
+            name = "Attach to process (codelldb)",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+        }
+      end
+    end,
   },
 }
