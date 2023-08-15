152333d741216db8791170cc1430578cd121e2bf
Filip Krikava <krikava@gmail.com>
2023-08-15 16:14:53 +0200

    Update duckdns script

--- bin/duckdns.sh
+++ bin/duckdns.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo url="https://www.duckdns.org/update?domains=f6k&token=61682be5-04a9-475b-a349-e951ba21b423&ip=" | curl -k -K -
+echo url="https://www.duckdns.org/update?domains=f6k-$(hostname)&token=61682be5-04a9-475b-a349-e951ba21b423&ip=" | curl -k -K -
