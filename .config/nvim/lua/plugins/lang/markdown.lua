2c35d0716d93d2940c1fbf9de7ff0e3e975eca7f
Filip Krikava <krikava@gmail.com>
2023-12-03 11:33:39 +0100

    nvim: add markdown preview

--- .config/nvim/lua/plugins/lang/markdown.lua
+++ .config/nvim/lua/plugins/lang/markdown.lua
@@ -1,3 +1,5 @@
+local Util = require("util")
+
 return {
   -- add to treesitter
   {
@@ -6,6 +8,16 @@ return {
       if type(opts.ensure_installed) == "table" then
         vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline" })
       end
+    end,
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    keys = { { "<localleader>P", Util.cmd("MarkdownPreviewToggle"), desc = "Preview toggle" } },
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
     end,
   },
 
