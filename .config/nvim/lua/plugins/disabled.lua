fa71d37c4243f1e4828ec8bebea7b66b915d372a
Filip Krikava <krikava@gmail.com>
2024-04-08 18:43:10 +0200

    Get back tree viewer

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
