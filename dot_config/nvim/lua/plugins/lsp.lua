ee57d785fe63afbdd5e4f70974f294aa749a37da
Filip Krikava <krikava@gmail.com>
2024-07-13 21:41:28 +0200

    Update rust configuration

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -9,22 +9,18 @@ return {
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "<M-CR>", vim.lsp.buf.code_action, mode = { "n", "i" }, desc = "Actions" }
-      keys[#keys + 1] =
-        { "<M-p>", vim.lsp.buf.signature_help, mode = { "n", "i" }, desc = "Signature Help", has = "signatureHelp" }
+      keys[#keys + 1] = {
+        "<M-p>",
+        vim.lsp.buf.signature_help,
+        mode = { "n", "i" },
+        desc = "Signature Help",
+        has = "signatureHelp",
+      }
     end,
-  },
-
-  {
-    "j-hui/fidget.nvim",
-    config = true,
-  },
-
-  -- prevent incompatibility issies with rust integartion
-  {
-    "neovim/nvim-lspconfig",
     opts = {
       setup = {
         rust_analyzer = function()
+          -- prevent incompatibility issies with rust integartion
           return true
         end,
       },
@@ -32,21 +28,34 @@ return {
   },
 
   {
+    "j-hui/fidget.nvim",
+    config = true,
+  },
+
+  {
     "mrcjkb/rustaceanvim",
     opts = {
-      default_settings = {
-        ["rust-analyzer"] = {
-          inlayHints = {
-            parameterHints = {
-              enable = false,
+      server = {
+        -- stylua: ignore
+        on_attach = function(_, bufnr)
+          vim.keymap.set("n", "<leader>cm", function() vim.cmd.RustLsp("expandMacro") end, { desc = "Expand Macro (Rust)", buffer = bufnr })
+          vim.keymap.set("n", "<leader>co", function() vim.cmd.RustLsp("openDocs") end, { desc = "Open Docs (Rust)", buffer = bufnr })
+          vim.keymap.set("n", "<leader>cC", function() vim.cmd.RustLsp("openCargo") end, { desc = "Open Cargo.toml (Rust)", buffer = bufnr })
+        end,
+        default_settings = {
+          ["rust-analyzer"] = {
+            inlayHints = {
+              parameterHints = {
+                enable = false,
+              },
+              typeHints = {
+                enable = false,
+              },
             },
-            typeHints = {
-              enable = false,
-            },
-          },
-          highlightRelated = {
-            references = {
-              enable = false,
+            highlightRelated = {
+              references = {
+                enable = false,
+              },
             },
           },
         },
