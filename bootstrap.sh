2188afdffeef51af80b0db58821d43445ab83f2d
Filip Krikava <krikava@gmail.com>
2024-10-14 18:21:47 -0400

    Fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if ! command chezmoi 2>&1; then
+if ! command -v chezmoi &>/dev/null; then
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin"
   export PATH=$PATH:"$HOME/.local/bin"
 fi
