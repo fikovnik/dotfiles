e7122e38ff46da5a81b46107e60002760435d680
Filip Krikava <krikava@gmail.com>
2024-10-15 21:36:38 -0400

    Pin stable NIX

--- bootstrap-tools.sh
+++ bootstrap-tools.sh
@@ -6,10 +6,14 @@ install_on_linux() {
   local nix_profile="$HOME/.nix-profile/etc/profile.d/nix.sh"
   if [[ -f "$nix_profile" ]]; then
     echo 'Nix is already installed'
+    . "$nix_profile"
   else
     sh <(curl -L https://nixos.org/nix/install) --no-daemon
+    . "$nix_profile"
+    nix-channel --remove nixpkgs
+    nix-channel --add https://nixos.org/channels/nixos-24.05 nixpkgs
+    nix-channel --update
   fi
-  . "$nix_profile"
   nix-env -if "$BASE_DIR/packages.nix"
 }
 
