208e75e1b007ed5961c9d828a7c7b1b626863c55
Filip Krikava <krikava@gmail.com>
2025-04-18 10:08:42 +0200

    Add .local/bin/transmission.sh

---
+++ dot_local/bin/executable_transmission.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+name=transmission
+
+# TODO: refactor to some docker utils
+function rm_stopped {
+  local name=$1
+  local state
+  state=$(status "$name")
+
+  if [[ "$state" == "false" ]]; then
+    docker rm "$name"
+  fi
+}
+
+function status {
+  local name="$1"
+  docker inspect -f "{{.State.Running}}" "$name" || echo "false"
+}
+
+function start {
+  local password
+  password=$(pass show api/nordvpn-openvpn | head -1)
+  local username
+  username=$(pass show api/nordvpn-openvpn | tail -1 | sed 's/username: //g')
+
+  docker run --cap-add=NET_ADMIN --device=/dev/net/tun -d \
+    -v "$HOME/NoBackup/transmission/:/data" \
+    -v /etc/localtime:/etc/localtime:ro \
+    -e "OPENVPN_PROVIDER=NORDVPN" \
+    -e "OPENVPN_USERNAME=$username" \
+    -e "OPENVPN_PASSWORD=$password" \
+    -e "PUID=$(id -u)" \
+    -e "PGID=$(id -g)" \
+    -e LOCAL_NETWORK=192.168.0.0/16 \
+    --log-driver json-file \
+    --log-opt max-size=10m \
+    --name "$name" \
+    -p 9091:9091 \
+    haugene/transmission-openvpn
+}
+
+case "$1" in
+start)
+  rm_stopped "$name"
+  start
+  ;;
+stop)
+  docker stop "$name"
+  ;;
+restart)
+  stop
+  start
+  ;;
+status)
+  status "$name"
+  ;;
+*)
+  echo "Usage: $0 <start|stop|restart|status>"
+  ;;
+esac
