71fb109b7223021f0aab8dbecc9dc3a83d7a9392
Filip Krikava <krikava@gmail.com>
2025-09-06 19:14:35 +0200

    Add .config/nvim/after/ftplugin/racket.lua Add .config/nvim/lua/plugins/racket.lua

---
+++ dot_config/nvim/after/ftplugin/racket.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("i", "<C-\\>", "λ", { buffer = true, desc = "Insert λ" })
---
+++ dot_config/nvim/lua/plugins/racket.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "commonlisp", "scheme", "racket" },
+    },
+  },
+  {
+    "eraserhd/parinfer-rust",
+    build = "cargo build --release",
+    ft = { "lisp", "scheme", "racket" },
+    cond = function()
+      return vim.fn.executable("cargo") == 1
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        racket_langserver = {},
+      },
+    },
+  },
+}
