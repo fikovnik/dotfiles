0a6ea0fe93ac996ca91dfecabe8382c5dc58b051
Filip Krikava <krikava@gmail.com>
2025-09-15 17:13:52 +0200

    Update .local/bin/raycast-open-tmux-terminal.sh Update .zimrc Update .zshrc

--- dot_local/bin/executable_raycast-open-tmux-terminal.sh
+++ dot_local/bin/executable_raycast-open-tmux-terminal.sh
@@ -2,7 +2,7 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title New tmux session
+# @raycast.title New tmux terminal
 # @raycast.mode silent
 
-open -a Ghostty "/Users/krikava/.local/bin/tmux-main.sh"
+open -a Ghostty "/Users/krikava/.local/bin/ghostty-new-terminal-tmux.sh"
