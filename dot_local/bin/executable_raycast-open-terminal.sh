6b6847d5b8ab80a3fb75d864ec1f47971af64991
Filip Krikava <krikava@gmail.com>
2025-09-18 20:51:44 +0200

    Add .local/bin/raycast-open-terminal.sh

---
+++ dot_local/bin/executable_raycast-open-terminal.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title New terminal
+# @raycast.mode silent
+
+open -a Ghostty "/Users/krikava/.local/bin/ghostty-new-terminal.sh"
