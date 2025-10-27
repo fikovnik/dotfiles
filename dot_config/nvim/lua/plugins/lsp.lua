51b241c19a5e2e76259ec4005e7ca99ce7902c13
Filip Krikava <krikava@gmail.com>
2025-10-27 07:38:23 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lsp.lua Update .zshenv

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,15 @@
-require("lazyvim.util").lsp.on_attach(function(_, _)
-  vim.opt_local.foldmethod = "expr"
-  vim.opt_local.foldexpr = "nvim_treesitter#foldexpr()"
-end)
-
 return {
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "<M-CR>", vim.lsp.buf.code_action, mode = { "n", "i" }, desc = "Actions" }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "<M-CR>", vim.lsp.buf.code_action, mode = { "n", "i" }, desc = "Say Hello" },
+          },
+        },
+      },
+    },
   },
 
   {
