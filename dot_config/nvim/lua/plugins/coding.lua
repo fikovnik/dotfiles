7834c6281c8f55cd2383aa76b4c10c337321bf69
Filip Krikava <krikava@gmail.com>
2025-12-18 20:28:17 +0100

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/coding.lua Update .config/nvim/lua/plugins/disabled.lua

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -8,66 +8,12 @@ return {
       vim.g.no_plugin_maps = true
     end,
     config = function()
-      require("nvim-treesitter-textobjects").setup({
-        select = {
-          lookahead = true,
-          selection_modes = {
-            ["@parameter.outer"] = "v",
-            ["@function.outer"] = "V",
-            ["@code_block.inner"] = "V",
-          },
-        },
-      })
-
-      -- keymaps
-      vim.keymap.set({ "x", "o" }, "af", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+      vim.keymap.set("n", "<M->>", function()
+        require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
       end)
-      vim.keymap.set({ "x", "o" }, "if", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "ac", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "ic", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "aC", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@block.outer", "textobjects")
-      end)
-      vim.keymap.set({ "x", "o" }, "iC", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@block.inner", "textobjects")
+      vim.keymap.set("n", "<M-lt>", function()
+        require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.inner")
       end)
     end,
   },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter-textobjects",
-  --   opts = {
-  --     textobjects = {
-  --       select = {
-  --         enable = true,
-  --         lookahead = true,
-  --         keymaps = {
-  --           ["aa"] = "@parameter.outer",
-  --           ["ia"] = "@parameter.inner",
-  --           ["af"] = "@function.outer",
-  --           ["if"] = "@function.inner",
-  --           ["ac"] = "@class.outer",
-  --           ["ic"] = "@class.inner",
-  --           ["iC"] = "@code_block.inner",
-  --           ["aC"] = "@code_block.outer",
-  --         },
-  --       },
-  --       swap = {
-  --         enable = true,
-  --         swap_next = {
-  --           ["<M->>"] = "@parameter.inner",
-  --         },
-  --         swap_previous = {
-  --           ["<M-<>"] = "@parameter.inner",
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
 }
--- dot_config/nvim/lua/plugins/disabled.lua
+++ dot_config/nvim/lua/plugins/disabled.lua
@@ -3,6 +3,6 @@ return {
   { "folke/noice.nvim", enabled = false },
   { "folke/persistence.nvim", enabled = false },
   { "nvim-mini/mini.pairs", enabled = false },
-  { "nvim-mini/mini.ai", enabled = false },
+  -- { "nvim-mini/mini.ai", enabled = false },
   -- { "nvim-mini/mini.icons", enabled = false },
 }
